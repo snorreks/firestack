8fc560ea5b1712a9c59dd050c4c6833bb088be5b
snorreks <snorrekstrand@hotmail.com>
2026-03-02 19:03:46 +0100

    Stop tracking tmp directory

--- example/apps/functions/tmp/created/created.ts
+++
@@ -1,4 +0,0 @@
-import { onDocumentCreated } from 'firebase-functions/firestore';
-import functionStart from '../../src/controllers/firestore/users/[uid]/created.ts';
-
-export const created = onDocumentCreated({}, functionStart);
--- example/apps/functions/tmp/updated/updated.ts
+++
@@ -1,4 +0,0 @@
-import { onDocumentUpdated } from 'firebase-functions/firestore';
-import functionStart from '../../src/controllers/firestore/users/[uid]/updated.ts';
-
-export const updated = onDocumentUpdated({}, functionStart);
