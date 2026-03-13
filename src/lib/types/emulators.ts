8fbdf153c99a03916b0c2d1f3e5ef32e5a33f220
snorreks <snorrekstrand@hotmail.com>
2026-03-13 03:58:10 +0100

    feat: add emulatorPorts

---
+++ src/lib/types/emulators.ts
@@ -0,0 +1,14 @@
+export type FirebaseEmulator =
+  | 'auth'
+  | 'functions'
+  | 'firestore'
+  | 'database'
+  | 'hosting'
+  | 'pubsub'
+  | 'storage'
+  | 'eventarc'
+  | 'extensions'
+  | 'ui'
+  | 'hub'
+  | 'logging'
+  | 'appcheck';
